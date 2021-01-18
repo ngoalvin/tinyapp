38bf0d5654b6493cd5179c06fcfee11e91dd0405
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-18 22:44:28 +0000

    added a redirect to long URL

--- express_server.js
+++ express_server.js
@@ -42,12 +42,16 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  // console.log(req.body);
   let id = generateRandomString();
   urlDatabase[id] = req.body.longURL 
   res.redirect(`/urls/${id}`);
