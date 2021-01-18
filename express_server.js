c3396a94a0f91953a10b1f6eaec0fbf749cb2f6b
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-18 19:13:26 +0000

    added header partial and added into url_index

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
