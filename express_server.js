e6050194826a115e4fa154c54bfcc5d5992dd850
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-19 21:23:14 +0000

    added cookie parser and login bar

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const cookieParser = require('cookie-parser');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
