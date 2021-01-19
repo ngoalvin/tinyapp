937ad8ea8a6ec9c6db58e8c7c7d35b7bca45d255
Alvin Ngo <ngoalvin97@gmail.com>
2021-01-19 17:58:39 +0000

    added delete button to EJS and post to delete the shortURL

--- express_server.js
+++ express_server.js
@@ -57,6 +57,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
